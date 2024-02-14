0549bad56c22da1899105830176d28c9b57a0f1a
moliva <oliva.miguelh@gmail.com>
2024-02-14 02:18:13 -0300

    feat: adds actions for current buffer

--- lua/private/actions.lua
+++ lua/private/actions.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+--- Triggers the action with the corresponding decryption algorithm for the current buffer file.
+--- @return boolean result Representing whether the operation was a success or not
+function M.encrypt_current_file()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local path = vim.api.nvim_buf_get_name(current_buf)
+
+  local result = require('private').encrypt(path)
+
+  if not result then
+    print("error while encrypting file '" .. path "'")
+  end
+
+  return result
+end
+
 --- Asks for input for the file to be encrypted and triggers the action with the corresponding encryption algorithm.
 --- @return boolean result Representing whether the operation was a success or not
 function M.encrypt()
@@ -13,6 +28,21 @@ function M.encrypt()
 
   if not result then
     print("error while encrypting file '" .. path "'")
+  end
+
+  return result
+end
+
+--- Triggers the action with the corresponding encryption algorithm for the current buffer file.
+--- @return boolean result Representing whether the operation was a success or not
+function M.decrypt_current_file()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local path = vim.api.nvim_buf_get_name(current_buf)
+
+  local result = require('private').decrypt(path, true)
+
+  if not result then
+    print("error while decrypting file '" .. path "'")
   end
 
   return result
